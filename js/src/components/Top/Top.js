9846a403195adf9f84692e380533eaafefc3207d
shinoda <tetora1053@gmail.com>
2019-12-17 13:21:04 +0900

    とりあえずコミット

--- js/src/components/Top/Top.js
+++ js/src/components/Top/Top.js
@@ -3,22 +3,14 @@ import './top.css';
 
 const Top = (props) => {
 
-  const incrementUserId = () => {
-    props.incrementUserId()
-  }
-
-  const incrementGroupId = () => {
-    props.incrementGroupId()
-  }
-
   return (
     <div>
       <p id="title">MyFavoriteMovies</p>
       <div>
-        <button onClick={incrementUserId}>increment UserID</button>{props.userId}
+        <button onClick={props.incrementUserId}>increment UserID</button>{props.userId}
       </div>
       <div>
-        <button onClick={incrementGroupId}>increment GroupID</button>{props.groupId}
+        <button onClick={props.incrementGroupId}>increment GroupID</button>{props.groupId}
       </div>
     </div>
   );
