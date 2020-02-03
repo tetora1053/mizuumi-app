2a23e1bed16cc381eb710f5493444f32b3735e1c
shinoda <tetora1053@gmail.com>
2020-02-04 02:50:12 +0900

    映画一覧用のMovieListコンポーネント作成。reducerも整理

--- js/src/containers/App.js
+++ js/src/containers/App.js
@@ -1,23 +1,13 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { getMovieById, getMoviesByUserId, getMovies } from '../actions'
+import { getMovieById } from '../actions'
 import AppComp from '../components/App/App'
-
-const mapStateToProps = state => {
-  return ({
-    movies: state.movies.movies,
-    userMovies: state.movies.userMovies
-  })
-}
 
 const mapDispatchToProps = (dispatch) => ({
   getMovieById: (id) => dispatch(getMovieById(id)),
-  getMovies: () => dispatch(getMovies()),
-  getMoviesByUserId: (user_id) => dispatch(getMoviesByUserId(user_id))
 })
 
 export default connect(
-  mapStateToProps,
   mapDispatchToProps
 )(AppComp)
 
--- js/src/reducers/reducer.js
+++ js/src/reducers/reducer.js
@@ -1,12 +1,12 @@
 import { combineReducers } from 'redux'
 import { connectRouter } from 'connected-react-router'
 import  movie from './movie'
-import  movies from './movies'
+import  movieTmbs from './movieTmbs'
 
 const createRootReducer = (history) => combineReducers({
     router: connectRouter(history),
     movie,
-    movies
+    movieTmbs
 })
 
 export default createRootReducer
